9508c36e48724cd799503b9f6bfbb4906e5ee1ec
Nathan Kinsey <nakinsey98@gmail.com>
2013-11-24 00:22:40 -0800

    Initial Commit (1.0.0)

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pomodorino
+---
+Pomodorino is a Pomodoro timer that stresses simplicity and functionality. The timer is based off of [the Pomodoro Technique](http://pomodorotechnique.com/) which was created by Francesco Cirillo. It's name comes from the Italian word for small tomato, as it is a light-weight version of the installable variations that circulate the web today.
+
+## About the Pomodoro Technique
+This productivity technique breaks up daunting work into easy 25 minute chunks called a "pomodoro." After each session of work, the user gets a short 3-5 minute break. For every four pomodori, the user is rewarded with a longer 10-15 minute break.
+
+In this light-weight flavor of the timer, a pomodoro will be refered to as a pomodorino. Additionally, the plural form of the word is "pomodorini."
+
+## TO DO:
+The current release of the timer is far from usable. It is bare-bones HTML and JavaScript. The top three goals for the project go as follows:
+
+1. Create an attractive, responsive UI that uses a flat design for ease-of-use
+2. Dynamically manipulate the `title` tag to show the timer's current state
+3. Track the number of pomodorini completed, to recommend what type of break should be taken.
+
+## Technologies Used
+This web application is crafted out of the following:
+- HTML
+- CSS
+- JavaScript
+ - jQuery
+
+## Licensing
+The Pomodorino timer uses the MIT license. You can [read it](http://opensource.org/licenses/MIT) on the Open Source Initiative's website.
---
+++ js/main.js
@@ -0,0 +1,86 @@
+var counter, minutes, second, seconds; // "second" is the interval and "seconds" is part of the secToMin(); function
+
+//-------------------- Time Unit Conversion Code ------------------------------------------------//
+
+var secToMin = function (input) {
+    "use strict";
+    minutes = String(Math.floor(input / 60));
+    seconds = input % 60;
+    // Leading zero for second values under 10
+    if (seconds < 10) {
+        String(seconds);
+        seconds = "0" + seconds;
+    } else {
+        String(seconds);
+    }
+    return minutes + ":" + seconds;
+};
+
+var minToSec = function (input) {
+    "use strict";
+    return input * 60;
+};
+
+//-------------------- Timer Code ---------------------------------------------------------------//
+
+var stopTimer = function () {
+    "use strict";
+    window.clearInterval(second);
+};
+
+var timer = function () {
+    "use strict";
+    $("title, #time").text(secToMin(counter));
+    if (counter === 0) {
+        stopTimer();
+        document.getElementById("alarm").play();
+    } else {
+        counter -= 1;
+    }
+};
+
+var startTimer = function () {
+    "use strict";
+    second = window.setInterval("timer()", 1000);
+};
+
+//-------------------- Pomomdoro Code -----------------------------------------------------------//
+
+var pomodorino = function () {
+    "use strict";
+    counter = 1500; // 25 Minutes
+    startTimer();
+};
+
+var shortBreak = function () {
+    "use strict";
+    counter = 300; // 5 Minutes
+    startTimer();
+};
+
+var longBreak = function () {
+    "use strict";
+    counter = 900; // 15 Minutes
+    startTimer();
+};
+
+//-------------------- User Interaction Code ----------------------------------------------------//
+
+$(document).ready(function () {
+    "use strict";
+    $("#pomodorino").click(function () {
+        pomodorino();
+    });
+    $("#sb").click(function () {
+        shortBreak();
+    });
+    $("#lb").click(function () {
+        longBreak();
+    });
+    $("#start").click(function () {
+        startTimer();
+    });
+    $("#stop").click(function () {
+        stopTimer();
+    });
+});
